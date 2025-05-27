eb6585d3bb27595d85acabe4041020e696b82bd5
azhar-azad <azhar.u.azad@gmail.com>
2025-05-27 10:12:36 +0600

    first commit

---
+++ src/test/java/com/pathfinders/SmartVoyagerAI/SmartVoyagerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathfinders.SmartVoyagerAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartVoyagerAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
