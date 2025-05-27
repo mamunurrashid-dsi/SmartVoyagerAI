05bc4e4f223a3967b497cf408d333e723edbbb24
Mamunur Rashid <mamunur.rashid@dsinnovators.com>
2025-05-27 16:54:05 +0600

    Updates prompt for current date and weather tool calling

--- src/main/java/com/pathfinders/SmartVoyagerAI/services/UserInputAgent.java
+++ src/main/java/com/pathfinders/SmartVoyagerAI/services/UserInputAgent.java
@@ -7,6 +7,8 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class UserInputAgent {
     private final ChatClient chatClient;
@@ -19,6 +21,7 @@ public class UserInputAgent {
         String systemPrompt = """
                 You are a travel assistant. Your job is to extract the following fields from the user's input:
                 - name
+                - current location
                 - destination
                 - startDate (Full date e.g 2025-05-27)
                 - endDate (Full date e.g 2025-06-30)
@@ -26,9 +29,12 @@ public class UserInputAgent {
                 - interests (comma-separated values like history, adventure, food, etc.)
                 - pace (relaxed, packed, or moderate)
                 
+                Current date is %s. So if user doesn't input exact date then use current date to figure out their
+                start data and end date of the tour.
+                
                 Return the result as a JSON object. If any field is missing or unclear, leave it as null.
                 If only month is provided in startDate and endDate append current year with that month (e.g July-2025).
-                """;
+                """.formatted(LocalDate.now());
         Prompt prompt = new Prompt(
                 new SystemMessage(systemPrompt),
                 new UserMessage(userInput)
@@ -37,8 +43,7 @@ public class UserInputAgent {
                 .call()
                 .content();
         assert response != null;
-        System.out.println("extracted json value \n"+response);
+        System.out.println("extracted json value \n"+response.substring(response.indexOf("```json"), response.lastIndexOf("```")));
         return response.substring(response.indexOf("```json"), response.lastIndexOf("```"));
     }
-
 }
