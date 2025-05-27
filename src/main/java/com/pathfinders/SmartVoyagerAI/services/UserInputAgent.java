9dc7682d2f48a167861de5a451001581ebf7ed00
Mamunur Rashid <mamunur.rashid@dsinnovators.com>
2025-05-27 17:00:13 +0600

    Updates prompt for current date and weather tool calling

--- src/main/java/com/pathfinders/SmartVoyagerAI/services/UserInputAgent.java
+++ src/main/java/com/pathfinders/SmartVoyagerAI/services/UserInputAgent.java
@@ -21,7 +21,7 @@ public class UserInputAgent {
         String systemPrompt = """
                 You are a travel assistant. Your job is to extract the following fields from the user's input:
                 - name
-                - current location
+                - currentLocation
                 - destination
                 - startDate (Full date e.g 2025-05-27)
                 - endDate (Full date e.g 2025-06-30)
