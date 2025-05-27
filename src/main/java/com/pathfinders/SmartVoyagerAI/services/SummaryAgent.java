cde3975fed8dd1e150dc93f3902f57eb3fedfee4
azhar-azad <azhar.u.azad@gmail.com>
2025-05-27 22:26:52 +0600

    System message updated

--- src/main/java/com/pathfinders/SmartVoyagerAI/services/SummaryAgent.java
+++ src/main/java/com/pathfinders/SmartVoyagerAI/services/SummaryAgent.java
@@ -45,10 +45,10 @@ public class SummaryAgent {
                 - Show weather info, flight info and hotel info. Use tools for these information.
                     - While using tools extract associated parameters for tool calling from userInput.
                     - While getting hotels, you need to get the cityCode from the destination from userInput and then call the tool.
+                    Include approximate costing on each suggested hotel and recommend which one is suitable for the budget in userInput.
                     - While getting flights, you need to get the originCityCode/IATA code from currentLocation and destinationCityCode/IATA code from destination
                     as well as startDate from userInput.
-                - All types of currency values should
-                 be converted according to currentLocation from userInput.
+                - All types of currency values should be converted according to currentLocation from userInput.
                 - Get an appropriate airport for the currentLocation on userInput.
                 - Create a personalized daily itinerary for the trip.
                 - Include recommendations for places to visit, things to do, and local food to try.
